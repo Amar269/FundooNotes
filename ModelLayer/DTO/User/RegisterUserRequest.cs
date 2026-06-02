38c1e931036d4c61c0d43394e3ba4b003a27d609
Amarnath Kolla <amarrama269@gmail.com>
2026-06-02 16:01:50 +0530

    created the DTO folde created two class flies which are RegisteRrUserRequest and UserResponse - in ModelLayer

---
+++ ModelLayer/DTO/User/RegisterUserRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO.User
+{
+    public  class RegisterUserRequest
+    {
+
+        [Required]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+       
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+        ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        
+        public string Password { get; set; }
+
+
+        
+    }
+}
---
+++ ModelLayer/DTO/User/UserResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO.User
+{
+    public class UserResponse // outgoing layer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+
+        [Required]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
+       
+
+
+
+
+    }
+}
