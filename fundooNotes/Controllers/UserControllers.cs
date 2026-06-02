c18fcc8f96bf91ae711a29efdf5ac1feb06d2b6a
Amarnath Kolla <amarrama269@gmail.com>
2026-06-02 17:54:43 +0530

    Implemented the Controllers and BusinessLogicLayer

---
+++ fundooNotes/Controllers/UserControllers.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogicLayer.Interface;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using ModelLayer.DTO.User;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace fundooNotes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserControllers : ControllerBase
+    {
+        private readonly IUserBLL _userBLL;
+
+        public UserControllers(IUserBLL userBLL)
+        {
+            _userBLL = userBLL;
+        }
+
+        [HttpPost]
+        public UserResponse RegsiterUser(RegisterUserRequest userRequest)
+        {
+            return _userBLL.RegisterUser(userRequest);
+        }
+        
+
+    }
+}
