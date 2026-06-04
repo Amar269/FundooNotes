ec48e2f96d803c9726a2414ef1597b6e1071fafe
Amarnath Kolla <amarrama269@gmail.com>
2026-06-04 15:44:32 +0530

    Created the DTO's for the notes table and Modellayer is udpated

---
+++ ModelLayer/DTO/Notes/UpdateNoteRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.DTO.Notes
+{
+    public  class UpdateNoteRequest // incoming layer 
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? Reminder { get; set; }
+
+        public string Colour { get; set; }
+
+        public string Image { get; set; }
+
+
+    }
+}
