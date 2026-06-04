fccac85a438d66ced9745b918e4ed631fac92d68
Amarnath Kolla <amarrama269@gmail.com>
2026-06-04 16:48:25 +0530

    implemented  the INoteBLL and NoteBLL in the BusinessLogicalayer

---
+++ BusinessLogicLayer/Interface/INoteBLL.cs
@@ -0,0 +1,15 @@
+﻿using ModelLayer.DTO.Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Interface
+{
+    public interface INoteBLL
+    {
+        NoteResponse CreateNote(CreateNoteRequest createNoteRequest, int userId);
+
+    }
+}
---
+++ BusinessLogicLayer/Service/NoteBLL.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLogicLayer.Interface;
+using DataBaseLayer.Interface;
+using ModelLayer.DTO.Notes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Service
+{
+    public class NoteBLL : INoteBLL
+    {
+        private readonly INoteDAL _noteDAL;
+
+        public NoteBLL(INoteDAL noteDAL)
+        {
+            _noteDAL = noteDAL;
+        }
+
+        public NoteResponse CreateNote(CreateNoteRequest createNoteRequest, int userId)
+        {
+            if (string.IsNullOrEmpty(createNoteRequest.Title))
+            {
+                throw new Exception("Title Required");
+            }
+
+            return _noteDAL.CreateNote(createNoteRequest, userId);
+
+        }
+    }
+}
